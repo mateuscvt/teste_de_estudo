8da83392cd4b2a383bd33e66a6e9307d5442a6d5
git_mateuscvt <mateuscavalcant886@gmail.com>
2026-06-25 19:49:54 -0300

    edição de teste

---
+++ teste.py
@@ -0,0 +1,12 @@
+class Mundo:
+    def __init__(self, planta, terra):
+       self.planta = planta
+       self.terra = terra
+    
+
+    def funcao(self):
+        if self.planta == 1:
+            pass
+
+    
+        
